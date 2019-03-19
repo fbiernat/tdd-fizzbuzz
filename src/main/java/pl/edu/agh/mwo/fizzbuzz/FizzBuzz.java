e2596a0fef2870e89f0d519dbb076a5e495dc0ea
filipbrnt <filipbiernat@gmail.com>
2019-03-19 15:31:47 +0100

    Add print() parameter for number of elements and change formatting

--- src/main/java/pl/edu/agh/mwo/fizzbuzz/FizzBuzz.java
+++ src/main/java/pl/edu/agh/mwo/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,12 @@
 package pl.edu.agh.mwo.fizzbuzz;
 
 public class FizzBuzz {
-	
+
+	public static void main(String[] args) {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		fizzbuzz.printn(100);
+	}
+
 	boolean isMultipleOf(int number, int... multiples) {
 		for (int multiple : multiples) {
 			if (number % multiple != 0)
@@ -9,7 +14,7 @@ public class FizzBuzz {
 		}
 		return true;
 	}
-	
+
 	public String getLine(int number) {
 		if (this.isMultipleOf(number, 3, 5)) {
 			return "FizzBuzz";
@@ -18,18 +23,15 @@ public class FizzBuzz {
 		} else if (this.isMultipleOf(number, 5)) {
 			return "Buzz";
 		}
-		return String.valueOf(number);
+		return "";
 	}
-	
-	public void print() {
-		for (int i = 1; i <= 100; i++) {
-			System.out.println(this.getLine(i));
+
+	public void printn(int n) {
+		for (int i = 1; i <= n; i++) {
+			System.out.printf("%3d %-8s ", i, this.getLine(i));
+			if (i % 5 == 0)
+				System.out.println("\n");
 		}
-	}
-	
-	public static void main(String[] args) {
-		FizzBuzz fizzbuzz = new FizzBuzz();
-		fizzbuzz.print();
 	}
 
 }
